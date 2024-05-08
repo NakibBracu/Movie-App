910a0cb19b8a2555b8a763c6dec8678ca5ae13c1
NakibBracu <nakibrahamanbracu18@gmail.com>
2024-05-08 12:43:24 +0600

    Methods Defined in the interface for unit tests!

--- Movie-App/Movie-App.Application/Services/IUserMovieService.cs
+++ Movie-App/Movie-App.Application/Services/IUserMovieService.cs
@@ -1,7 +1,14 @@
-﻿namespace Movie_App.Application.Services
+﻿using Movie_App.Domain.Entities;
+using Movie_App.Domain.Interfaces;
+
+namespace Movie_App.Application.Services
 {
     public interface IUserMovieService
     {
+        void AddFavoriteMovie(User user, IMovie movie);
+        void RemoveFavoriteMovie(User user, IMovie movie);
+        IEnumerable<IMovie> GetFavoriteMovies(User user);
+        IEnumerable<IMovie> SearchMovies(User user, string keyword);
 
     }
 }
