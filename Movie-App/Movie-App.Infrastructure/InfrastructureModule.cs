b15cfb9c4662433209691e431fb7e1dc19b8a3e6
NakibBracu <nakibrahamanbracu18@gmail.com>
2024-05-08 12:21:56 +0600

    Infrastructure layer added where service implementation will be existed

---
+++ Movie-App/Movie-App.Infrastructure/InfrastructureModule.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using Movie_App.Application.Services;
+using Movie_App.Infrastructure.Services;
+
+namespace Movie_App.Infrastructure
+{
+    public class InfrastructureModule : Module
+    {
+        public InfrastructureModule()
+        {
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<UserMovieService>().As<IUserMovieService>()
+                .InstancePerLifetimeScope();
+
+            base.Load(builder);
+        }
+    }
+}
---
+++ Movie-App/Movie-App.Infrastructure/Services/UserMovieService.cs
@@ -0,0 +1,18 @@
+﻿using Movie_App.Application.Services;
+using Movie_App.Persistence.Repository;
+
+namespace Movie_App.Infrastructure.Services
+{
+    public class UserMovieService : IUserMovieService
+    {
+        private readonly IUserRepository userRepository;
+        private readonly IMovieRepository movieRepository;
+
+        public UserMovieService(IUserRepository userRepository, IMovieRepository movieRepository)
+        {
+            this.userRepository = userRepository;
+            this.movieRepository = movieRepository;
+        }
+
+    }
+}
